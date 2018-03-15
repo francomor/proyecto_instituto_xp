4d34ba01d7c78b97abe86f51b728dd19501bdb6c
Nicolás Silvera <nico.silvera.fi@gmail.com>
2018-03-15 19:17:50 -0300

    Corrección de lo anterior
    Había una columna desviada
    
    Co-Authored-By: gabyperalta <gabyperalta@users.noreply.github.com>

--- logica/listarInasistencias.php
+++ logica/listarInasistencias.php
@@ -5,24 +5,24 @@ $dni_alumno=$_POST["dni"];
 $resultado=$asistencia -> listar_inasistencia($dni_alumno);
 ?>
 <html>
-	<body>
-	<h5>  Boletin de inasistencias</h3> <hr>
+  <body>
+  <h3>  Boletin de inasistencias</h3> <hr>
             
- 			<div class="container">    
- 			  <table class="table table-striped">
- 			    <thead>
- 			      <tr>
- 			        <th>Fecha</th><td></td>
- 			        <th>Falto a</th><td></td>
- 			        <th>Falta</th><td></td>
- 			        <th>Causas de inasistencia</th><td></td>
- 			      	<th>Firma</th><td></td>
- 			      	<th>Total</th><td></td>
- 			      </tr>
- 			    </thead>
- 			    <tbody>
+      <div class="container">    
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Fecha</th><td></td>
+              <th>Falto a</th><td></td>
+              <th>Falta</th><td></td>
+              <th>Causas de inasistencia</th><td></td>
+              <th>Firma</th><td></td><td></td>
+              <th>Total</th><td></td>
+            </tr>
+          </thead>
+          <tbody>
 <?php
-	$acumulado=0; 
+  $acumulado=0; 
   foreach($resultado as $row)
   {
     $fecha=$row["fecha"];
@@ -34,12 +34,15 @@ $resultado=$asistencia -> listar_inasistencia($dni_alumno);
     echo "<td>".$falta."<td>";
     echo "<td></td>";
     echo "<td></td>";
+    echo "<td></td>";
+    echo "<td></td>";
+    echo "<td></td>";
     echo "<td>".$acumulado."<td>";
     echo "<tr>";
   }
 ?>           
- 			    </tbody>
- 			  </table>
- 			</div>
- 	</body>
+          </tbody>
+        </table>
+      </div>
+  </body>
  </html>
