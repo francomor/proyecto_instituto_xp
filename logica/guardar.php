3453bc45c1eee364e4a7b906c4df66a0245075e6
francomor <francomorero96@gmail.com>
2018-03-20 16:36:41 -0300

    justificado 0
    acomodado para integrar con los demas
    
    Co-Authored-By: herrerofrancisco <herrerofrancisco@users.noreply.github.com>

--- logica/guardar.php
+++ logica/guardar.php
@@ -18,19 +18,19 @@ for ($i = 0; $i < $cant_filas; $i++) {
         $falta_ed_f = $_REQUEST[(String) ($i + 1) . 'ed-f_aus']; //obtengo si falto o no a edfisica
         if ($falta_clase == NULL && $falta_ed_f == NULL) { //si falto a clase y edfisica...
             $valor_total = 1;
-            $a->cargar_asistencia($fecha,"clase+edfisica", $valor_total, $dni, $curso, 1);
+            $a->cargar_asistencia($fecha,"clase+edfisica", $valor_total, $dni, $curso, 0);
         } else if ($falta_clase == NULL) { //si falto a clase
             $valor_total = "1/2";
-            $a->cargar_asistencia($fecha,"clase", $valor_total, $dni, $curso, 1);
+            $a->cargar_asistencia($fecha,"clase", $valor_total, $dni, $curso, 0);
         } else if ($falta_ed_f == NULL) { //si falto a ed fisica...
             $valor_total = "1/2";
-            $a->cargar_asistencia($fecha,"edfisica", $valor_total, $dni, $curso, 1);
+            $a->cargar_asistencia($fecha,"edfisica", $valor_total, $dni, $curso, 0);
         }
     } else { //si el dia que se computa la asistencia hay solo clase...
         $a = new Asistencia();
         if ($falta_clase == NULL) { //si falto a clase...
             $valor_total = 1;
-            $a->cargar_asistencia($fecha,"clase", $valor_total, $dni, $curso, 1);
+            $a->cargar_asistencia($fecha,"clase", $valor_total, $dni, $curso, 0);
         }
     }
     echo ("<script>");
