41d6d846dae1e3f902658be9d37abf0a91977712
Francisco <herrero.f96@gmail.com>
2018-05-28 13:31:19 -0300

    Modificaciones en tarea de Alta de alumnos y mostrar alumnos

---
+++ presentacion/seleccionarCursoMostrarAlumnos.php
@@ -0,0 +1,80 @@
+<?php
+require_once "GUIPreceptor.class.php";
+
+if (isset($_SESSION["login"]) && $_SESSION["login"] == true) {
+
+      /**
+       * Seleccionar curso
+       * @author 
+       * @version 1.0
+       */
+      $gui_preceptor = new GUIPreceptor();
+
+      ?>
+          
+        <div class="content-wrapper">
+       
+          <section class="content-header">
+            <h1>
+              MOSTRAR ALUMNOS
+              <small> </small>
+            </h1> 
+             <ol class="breadcrumb">
+              <li><a href="home.php"><i class="fa fa-dashboard"></i> Home</a></li>
+              <li class="active">Seleccionar Curso</li>
+            </ol>
+          </section>
+          
+         
+         <section class="content">
+            <div class="row">
+              <div class="col-xs-12">
+                <div class="box">
+                  <div class="box-header">
+                    <h3 class="box-title">Seleccione un curso</h3>
+                  </div>
+                  <!-- /.box-header -->
+                <div class="box-body">
+            
+            
+
+          <body onload="cargar()"> <!--cuando se carga el body se ejecuta el cargar cursos -->
+
+
+           <div id="aca">
+              <!--Lista de cursos  -->
+           </div>
+          
+
+            
+
+          <script>
+              function cargar() //funcion ajax para traer desde cargarCursosEdF-php todos los cursos de la bdd, para luego cargarlo en el select
+              {
+                  var xmlhttp = new XMLHttpRequest();
+                  xmlhttp.onreadystatechange = function () {
+                      if (this.readyState == 4 && this.status == 200) {
+                          document.getElementById("aca").innerHTML = this.responseText;
+                      }
+                  };
+                  xmlhttp.open("GET", "../logica/cargarCursosEdF.php?funcion=mostrarAlumnos", true);
+                  xmlhttp.send();
+              }
+          </script>
+       </div>
+       </div>
+       </div>
+       
+          </section>
+
+       
+      </div>
+       
+      <?php
+      //Agrega el footer comun a todas las secciones
+      $gui_preceptor->cargarFooter();
+
+} 
+else {
+header('location: ../presentacion/login.php');
+}
