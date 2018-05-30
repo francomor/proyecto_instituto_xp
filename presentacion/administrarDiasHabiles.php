e48ffe6f6517f6c24da1ed147781a7245e7a04be
francomor <francomorero96@gmail.com>
2018-05-30 10:06:39 -0300

    Arreglado vista de dias habiles
    Co-Authored-By: karenariananavarro <karenariananavarro@users.noreply.github.com>

--- presentacion/administrarDiasHabiles.php
+++ presentacion/administrarDiasHabiles.php
@@ -9,76 +9,93 @@ require_once "GUIPreceptor.class.php";
 $gui_preceptor = new GUIPreceptor();
 ?>
 
+
 <div class="content-wrapper">
 
-  <section class="content-header">
-    <h1>
+       <section class="content-header">
+              <h1>
+                  ADMINISTRAR DÍAS HÁBILES
+              </h1>
+              <ol class="breadcrumb">
+                <li><a href="home.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
+                <li class="active">Administrar días hábiles</li>
+              </ol> 
+        </section>
+        <section class="content">
+       
+           <div class="panel panel-default">
+            <div class="panel-heading">
+                  <h4>Seleccione días no hábiles</h4>
+            </div>
+            <div class="alert alert-danger">
+                    <strong>Los dias habiles se cargan automáticamente al cargar una inasistencia. Solo agregue en caso de anomalía, o elimine en casos de errores.</strong>
+            </div>
 
-      <small> </small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="home.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
-      <li><a href="registrarAsistencia.php">Asistencias</a></li>
-      <li class="active">Registrar asistencias</li>
-    </ol> 
-  </section>
-<div style="text-align: center; background-color: cyan;"> Los dias habiles se cargan automáticamente al cargar una inasistencia, solo agregue en caso de anomalía, o elimine en casos de errores.</div>
+            <div class="panel-body">
 
- <form action="../logica/modificarDiasHabiles.php" method="post" id="formularioDH">
-	<table class="table table-bordered table-hover" border="1"> 
-              <?php
-                $dhabiles=new DiasHabiles();
-                $dias=$dhabiles->recuperarOrdenado();
-                $cantfilas = count($dias);
-                $fActual= date("Y")."-".date("m")."-".date("d");
-              ?>
+                <form action="../logica/modificarDiasHabiles.php" method="post" id="formularioDH">
+                    <table class="table table-bordered table-hover" border="1"> 
+                                <?php
+                                  $dhabiles=new DiasHabiles();
+                                  $dias=$dhabiles->recuperarOrdenado();
+                                  $cantfilas = count($dias);
+                                  $fActual= date("Y")."-".date("m")."-".date("d");
+                                ?>
 
- 	        <?php
-          for ($i = 0; $i < $cantfilas; $i++) {
-                    //se hace de manera dinamica la carga de los dias a la tabla, con sus respectivos 
-                    //checkbox donde un checkbox marcado es un día habil.
-          ?>
-            <tr>
-              <td width="20%">
-                <?php
-                switch (date('w', strtotime($dias[$i]['fecha']))){ 
-                  case 0: echo "Domingo"; break; 
-                  case 1: echo "Lunes"; break; 
-                  case 2: echo "Martes"; break; 
-                  case 3: echo "Miercoles"; break; 
-                  case 4: echo "Jueves"; break; 
-                  case 5: echo "Viernes"; break; 
-                  case 6: echo "Sabado"; break; 
-                  } 
-                ?>
-              </td>
-              <td width="75%"> 
-              <?php
-              $auxFecha=$dias[$i]['fecha'];
-              $partes = explode('-', $auxFecha);                      //Es mas legible si damos vuelta la fecha
-              $fecha = "{$partes[2]}-{$partes[1]}-{$partes[0]}";
-              echo "{$partes[2]}-{$partes[1]}-{$partes[0]}";
-              ?> </td>   <!-- se muestra el dia habil -->
-           </tr>
+                            <?php
+                            for ($i = 0; $i < $cantfilas; $i++) {
+                                      //se hace de manera dinamica la carga de los dias a la tabla, con sus respectivos 
+                                      //checkbox donde un checkbox marcado es un día habil.
+                            ?>
+                              <tr>
+                                <td width="20%">
+                                  <?php
+                                  switch (date('w', strtotime($dias[$i]['fecha']))){ 
+                                    case 0: echo "Domingo"; break; 
+                                    case 1: echo "Lunes"; break; 
+                                    case 2: echo "Martes"; break; 
+                                    case 3: echo "Miercoles"; break; 
+                                    case 4: echo "Jueves"; break; 
+                                    case 5: echo "Viernes"; break; 
+                                    case 6: echo "Sabado"; break; 
+                                    } 
+                                  ?>
+                                </td>
+                                <td width="75%"> 
+                                <?php
+                                $auxFecha=$dias[$i]['fecha'];
+                                $partes = explode('-', $auxFecha);                      //Es mas legible si damos vuelta la fecha
+                                $fecha = "{$partes[2]}-{$partes[1]}-{$partes[0]}";
+                                echo "{$partes[2]}-{$partes[1]}-{$partes[0]}";
+                                ?> </td>   <!-- se muestra el dia habil -->
+                             </tr>
 
-           <?php
-                }//cierre del for
-                ?>
-    </table>
-    <table">
-          <tr>
-            <td colspan="2">
-              <input name="fecha" id="fecha" type="date" value="<?php echo date('Y-m-d', strtotime($fActual)) ?>"  required>
-            </td>
-           <td colspan="2">
-            <input type="submit" class="btn btn-primary" value="Agregar" name="agregar" id="agregar">
-           </td>
-             <td colspan="2">
-            <input type="submit" class="btn btn-primary" value="Quitar" name="quitar" id="quitar" onclick="quitarMarcados()">
-           </td>
-           </tr>
-    </table>
-	</form>
+                             <?php
+                                  }//cierre del for
+                                  ?>
+                      </table>
+                      <table>
+                            <tr>
+                              <td colspan="2">
+                                <input style="margin: 10px 5px" name="fecha" id="fecha" type="date" value="<?php echo date('Y-m-d', strtotime($fActual)) ?>"  required>
+                              </td>
+                             <td colspan="2">
+                              <input style="margin:8px 3px" type="submit" class="btn btn-danger" value="Agregar" name="agregar" id="agregar">
+                             </td>
+                               <td colspan="2">
+                              <input style="margin:8px 3px" type="submit" class="btn btn-danger" value="Quitar" name="quitar" id="quitar" onclick="quitarMarcados()">
+                             </td>
+                             </tr>
+                      </table>
+              </form>
+
+          </div>
+        </div>
+
+      </section>
+       
+      
+
 </div>
 
 
--- presentacion/formularioListarInasistencias.php
+++ presentacion/formularioListarInasistencias.php
@@ -11,7 +11,7 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] == true) {
      */
     $gui_preceptor = new GUIPreceptor();
     ?>
-          <div class="content-wrapper">
+      <div class="content-wrapper">
 
             <section class="content-header">
               <h1>BUSCAR INASISTENCIAS POR DNI</h1>
@@ -21,24 +21,26 @@ if (isset($_SESSION["login"]) && $_SESSION["login"] == true) {
               </ol>
             </section>
 
-            <section class="content">
+           <section class="content">          
+             <div class="panel panel-default">
+              <div class="panel-heading">
+                        <h4>Ingrese DNI del alumno</h4>
+              </div>
 
 
-              <form class="form-horizontal" method="POST" action="../presentacion/listarInasistencias.php" role="form">
-                <div class="box">
-                  <div class="box-header">
-                    <h3 class="box-title">Ingrese DNI del alumno</h3>
-                  </div>
-                  <div class="form-inline" style="padding: 0 15px 15px;">
-                    <label style="margin: 5px 10px" for="dni">DNI</label>
-                    <input type="text" style="width: 200px; margin: 5px 3pxx" class="form-control" id="dni" name="dni" size="30" pattern="[0-9]{8}" required>
-                    <button class="btn btn-danger" style="margin: 5px 3px" type="submit">Buscar</button>
-                  </div>
-                </div>
-              </form>
-            </section>
+                    <form class="form-horizontal" method="POST" action="../presentacion/listarInasistencias.php" role="form">
+                      <div class="panel-body">
+                          <div class="form-inline" style="padding: 0 15px 15px;">
+                            <label style="font-family: sans-serif; margin-right: 8px;">DNI</label>
+                            <input type="text" style="width: 200px; margin: 5px 3pxx" class="form-control" id="dni" name="dni" size="30" pattern="[0-9]{8}" required>
+                            <button class="btn btn-danger" style="margin: 5px 3px" type="submit">Buscar</button>
+                          </div>
+                      </div>
+                    </form>
+            </div>
+          </section>
 
-          </div>
+    </div>
 
 
         <?php
